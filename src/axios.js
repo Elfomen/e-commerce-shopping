fa7b5691c8cff658fa76877e3752932b0a3747c1
elfomen <elfomenprofessor@gmail.com>
2022-07-11 19:48:35 +0100

    linked the axios base url to the heroku application online

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const event = axios.create({
-    baseURL : "http://127.0.0.1:8000/" ,
+    baseURL : "https://ecommerceapi-shopapp.herokuapp.com/" ,
     
     //baseURL : "http://localhost:5000"
 })
